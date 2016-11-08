cf18783352a000f772135341a6b61c128bce47ea
K0k1Nakamura <k00ook17@gmail.com>
2016-11-08 14:54:24 +0900

    Update utils/data.py

--- ML/data.py
+++ ML/data.py
@@ -20,33 +20,34 @@ def load_image_list(path):
     return tuples
 
 
-def random_brightness(image, max_delta=63, seed=None):
+def random_brightness(image, max_delta=0.5, seed=None):
     delta = np.random.uniform(-max_delta, max_delta)
-    newimg = image + delta
+    newimg = np.clip(image * delta, 0, 255)
     return newimg
 
 
-def random_contrast(image, lower, upper, seed=None):
-    f = np.random.uniform(-lower, upper)
+def random_contrast(image, lower=0.2, upper=1.8, seed=None):
+    f = np.random.uniform(lower, upper)
     mean = (image[0] + image[1] + image[2]).astype(np.float32) / 3
     ximg = np.zeros(image.shape, np.float32)
     for i in range(0, 3):
         ximg[i] = (image[i] - mean) * f + mean
+    ximg = np.clip(ximg, 0, 255)
     return ximg
 
 
-def image_whitening(img):
-    img = img.astype(np.float32)
-    d, w, h = img.shape
-    num_pixels = d * w * h
-    mean = img.mean()
-    variance = np.mean(np.square(img)) - np.square(mean)
-    stddev = np.sqrt(variance)
-    min_stddev = 1.0 / np.sqrt(num_pixels)
-    scale = stddev if stddev > min_stddev else min_stddev
-    img -= mean
-    img /= scale
-    return img
+# def image_whitening(img):
+#     img = img.astype(np.float32)
+#     d, w, h = img.shape
+#     num_pixels = d * w * h
+#     mean = img.mean()
+#     variance = np.mean(np.square(img)) - np.square(mean)
+#     stddev = np.sqrt(variance)
+#     min_stddev = 1.0 / np.sqrt(num_pixels)
+#     scale = stddev if stddev > min_stddev else min_stddev
+#     img -= mean
+#     img /= scale
+#     return img
 
 
 class Data(object):
@@ -61,30 +62,34 @@ class Data(object):
         self.crop_noize = 7
         self.TEST_N = len(self.test)
 
-    def read_image(self, path, flip=True):
+    def read_image(self, path, train=True):
         # Data loading routine
-        resize_img = Image.open(path).resize((self.insize + self.crop_noize
-                                              , self.insize + self.crop_noize))
-        img = np.asarray(resize_img).transpose(2, 0, 1)
-
         # random crop
-        top = random.randint(0, self.crop_noize)
-        left = random.randint(0, self.crop_noize)
-        bottom = self.insize + top
-        right = self.insize + left
-        image = img[:, top:bottom, left:right].astype(np.float32)
+        if train:
+            resize_img = Image.open(path).resize((self.insize + self.crop_noize
+                                                  , self.insize + self.crop_noize))
+            img = np.asarray(resize_img).transpose(2, 0, 1)
+            top = random.randint(0, self.crop_noize)
+            left = random.randint(0, self.crop_noize)
+            bottom = self.insize + top
+            right = self.insize + left
+            image = img[:, top:bottom, left:right].astype(np.float32)
+        else:
+            resize_img = Image.open(path).resize((self.insize, self.insize))
+            img = np.asarray(resize_img).transpose(2, 0, 1)
+            image = img.astype(np.float32)
 
         # left-right flipping
-        if flip and random.randint(0, 1) == 0:
+        if train and random.randint(0, 1) == 0:
             image = image[:, :, ::-1]
         # random brightness
-        if random.randint(0, 4) != 0:
+        if train and random.randint(0, 4) != 0:
             image = random_brightness(image)
         # random contrast
-        if random.randint(0, 4) != 0:
-            image = random_contrast(image, lower=0.2, upper=1.8)
+        if train and random.randint(0, 4) != 0:
+            image = random_contrast(image)
         # whitening
-        image = image_whitening(image)
+        # image = image_whitening(image)
 
         image /= 255
         return image
@@ -104,7 +109,8 @@ class Data(object):
         for k, idx in enumerate(index):
             path = data_set[idx][0]
             target = data_set[idx][1]
-            img = self.read_image(path)
+            train = not test
+            img = self.read_image(path, train=train)
             #TODO: tolist may be very slow!
             x_batch[k] = img
             t_batch[k] = target
